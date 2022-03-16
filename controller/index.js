dd9e35845d38c248201e1396eb9ff911a8cbadd9
pontespablo <pablo.pontes@fatec.sp.gov.br>
2022-03-16 11:12:03 -0300

    index

--- controller/index.js
+++ controller/index.js
@@ -2,7 +2,23 @@ const express = require("express");
 
 const app = express();
 
+app.get("/", function(req, resp){
+    resp.send("Bem vindo ao meu App");
+})
+
+app.get("/Contato", function(req, resp){
+    resp.send("Página de Contato do meu App");
+})
+
+app.get("/Produto", function(req, resp){
+    resp.send("Página de Produtos do meu App");
+})
+
+app.get("/dados/:nome/:cargo", function(req, resp){
+    resp.send("<h1>Olá sr(a). "+req.params.nome+"</h1><h2>Seu Cargo é " +req.params.cargo+"</h2>");
+})
+
 // esta sempre deve ser a ultima linha quando usamos o express
-app.listen(8081, function(){
-    console.log("Servidor funcionando na url http://localhost:8081");
+app.listen(8080, function(){
+    console.log("Servidor funcionando na url http://localhost:8080");
 });
